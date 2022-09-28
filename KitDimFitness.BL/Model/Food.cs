6dfc45f26047ac610326a179ac588b30ea7bdf43
kitdim <kitaykin9808@gmail.com>
2022-09-28 21:03:59 +0300

    Добавил модель для подчета информации о каллариях жиров углеводы белки

---
+++ KitDimFitness.BL/Model/Food.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitDimFitness.BL.Model
+{
+    public class Food
+    {
+        public string Name { get; }
+        /// <summary>
+        /// Белки
+        /// </summary>
+        public double Proteins { get; }
+        /// <summary>
+        /// Жиры
+        /// </summary>
+        public double Fats { get; }
+        /// <summary>
+        /// Углеводы
+        /// </summary>
+        public double Carbohydrates { get; }
+        /// <summary>
+        /// Калории
+        /// </summary>
+        public double Callories { get; }
+
+        public Food(string name) : this(name, 0, 0, 0, 0) { }
+        public Food(string name, double proteins, double fats, double carbohyds, double callories)
+        {
+            // TODO: ПРОВЕРКА
+
+            Name = name;
+            Proteins = proteins / 100.0;
+            Fats = fats / 100.0;
+            Carbohydrates = carbohyds / 100.0;
+            Callories = callories / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Name; 
+        }
+    }
+}
