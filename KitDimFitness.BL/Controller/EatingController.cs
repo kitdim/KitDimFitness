36af31cf65cace3ebb1921b59ab05fc0b69e565e
kitdim <kitaykin9808@gmail.com>
2022-09-28 21:02:25 +0300

    Добавил класс котроллера для приема пищи

---
+++ KitDimFitness.BL/Controller/EatingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using KitDimFitness.BL.Model;
+
+namespace KitDimFitness.BL.Controller
+{
+    public class EatingController
+    {
+        private readonly User user;
+        public List<Food> Foods { get; }
+        public EatingController(User user)
+        {
+            this.user = user ?? throw new ArgumentNullException("Пользователь не может быть пустым.",nameof(user));
+            Foods = GetAllFoods();
+        }
+
+        private List<Food> GetAllFoods()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("foods.dat", FileMode.OpenOrCreate))
+            {
+                if (fs.Length > 0 && formatter.Deserialize(fs) is List<Food> foods)
+                {
+                    return foods;
+                }
+                else
+                {
+                    return new List<Food>();
+                }
+            }
+        }
+    }
+}
